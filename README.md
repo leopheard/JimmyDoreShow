94034e0488b8af248f0eeec5f1647b0e9ac346cd
leopheard <leopheard.misc@gmail.com>
2019-07-12 14:13:26 -0400

    comedy section typo

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 #The Jimmy Dore Show podcasts<br>
 =============================
 
-KODI / XBMC / Firestick addon for the Jimmy Dore Show podcasts
+KODI / XBMC / Firestick audio addon for the Jimmy Dore Show podcasts<br>
 
-<a href="https://jimmydorecomedy.com/">JimmyDoreComedy.com</a>
+<a href="https://jimmydorecomedy.com/">JimmyDoreComedy.com</a><br>
 
-#JimmyDoreshow
-#podcast
-
-To install this addon, either use the Kodi Github installer addon or save the .zip file downloaded from the 'clone or download' button to somewhere the Kodi can access. Then on the Kodi, go to addons > install from zip file.
+#JimmyDoreshow<br>
+#podcast<br>
 
 <a href="https://jimmydorecomedy.com/"><img src="https://upisnvej69-flywheel.netdna-ssl.com/wp-content/uploads/2017/06/slider4c.jpg">
--- addon.py
+++ addon.py
@@ -1,9 +1,10 @@
 from xbmcswift2 import Plugin, xbmcgui
-from resources.lib import kimiversenshow
+from resources.lib import jimmydoreshow
 
 plugin = Plugin()
 
-URL = "https://feed.podbean.com/kimiversen/feed.xml"
+URL = "https://thejimmydoreshow.libsyn.com/rss"
+URL2 = "https://comedyandeverythingelse.libsyn.com/rss"
 
 @plugin.route('/')
 def main_menu():
@@ -14,20 +15,16 @@ def main_menu():
         {
             'label': plugin.get_string(30000), 
             'path': plugin.url_for('all_episodes'),
-            'thumbnail': "https://pbcdn1.podbean.com/imglogo/ep-logo/pbblog4541467/Untitled-1.jpg"},
+            'thumbnail': "https://ssl-static.libsyn.com/p/assets/b/0/4/3/b043eaa3e36ed529/JDPodThumb1.jpg"},
    {
             'label': plugin.get_string(30001), 
             'path': plugin.url_for('all_episodes1'),
-<<<<<<< HEAD
             'thumbnail': "https://ssl-static.libsyn.com/p/assets/b/0/4/3/b043eaa3e36ed529/JDPodThumb1.jpg"},
    {
             'label': plugin.get_string(30002), 
             'path': plugin.url_for('all_comedy'),
             'thumbnail': "https://ssl-static.libsyn.com/p/assets/7/9/5/3/7953b2f961f84fe3/caee-for-podcast.jpg"},
 
-=======
-            'thumbnail': "https://pbcdn1.podbean.com/imglogo/ep-logo/pbblog4541467/Untitled-1.jpg"},
->>>>>>> 038dd81bd86778d95a5c90da02264a017375cfa5
     ]
 
     return items
@@ -38,11 +35,11 @@ def all_episodes():
     """
     contains playable podcasts listed as just-in
     """
-    soup = kimiversenshow.get_soup(URL)
+    soup = jimmydoreshow.get_soup(URL)
     
-    playable_podcast = kimiversenshow.get_playable_podcast(soup)
+    playable_podcast = jimmydoreshow.get_playable_podcast(soup)
     
-    items = kimiversenshow.compile_playable_podcast(playable_podcast)
+    items = jimmydoreshow.compile_playable_podcast(playable_podcast)
 
     return items
 
@@ -52,11 +49,25 @@ def all_episodes1():
     """
     contains playable podcasts listed as just-in
     """
-    soup = kimiversenshow.get_soup(URL)
+    soup = jimmydoreshow.get_soup(URL)
     
-    playable_podcast1 = kimiversenshow.get_playable_podcast1(soup)
+    playable_podcast1 = jimmydoreshow.get_playable_podcast1(soup)
     
-    items = kimiversenshow.compile_playable_podcast1(playable_podcast1)
+    items = jimmydoreshow.compile_playable_podcast1(playable_podcast1)
+
+    return items
+
+
+@plugin.route('/all_comedy/')
+def all_comedy():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup2 = jimmydoreshow.get_soup(URL2)
+    
+    playable_comedy = jimmydoreshow.get_playable_comedy(soup2)
+    
+    items = jimmydoreshow.compile_playable_comedy(playable_comedy)
 
     return items
 
